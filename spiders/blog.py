313011337e7e7d1747e1adbcdce3f208b2b96777
yezhwi <yzhw.2008@163.com>
2018-05-31 13:57:44 +0800

    extract_first

--- spiders/blog.py
+++ spiders/blog.py
@@ -11,8 +11,9 @@ class BlogSpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//div[@class="post-preview"]'):
             item = DemoItem()
-            # return list
+            # extract()返回的也是一个列表
             # item['title'] = sel.xpath('.//h2/text()').extract()
+            # extract_first()可以直接返回第一个值，extract_first()有一个参数default,如：extract_first(default="")表示如果匹配不到返回一个空
             item['title'] = sel.xpath('.//h2/text()').extract_first("").strip()
             item['link'] = response.url + sel.xpath('.//a/@href').extract_first("")
             yield item
